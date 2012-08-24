338f652d7b5b1f82ce442411b3924c557b869333
Ron <ron@skullsecurity.net>
2012-08-23 22:02:23 -0500

    Added a script to make it easy to install the 'addition' (ie, drivers) to VBox

---
+++ vbox-install-additions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME="$1"
+
+VBOX="/usr/bin/VBoxManage"
+VBOXUI="/usr/bin/VBoxSDL"
+VBOXADDITIONS="/vmware/iso/VBoxGuestAdditions_4.1.18.iso"
+VM_DIR="/vmware/VirtualBox/$NAME"
+
+die()
+{
+  $VBOX unregistervm "$NAME" --delete
+  echo "ERROR: ${1}"
+  exit
+}
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <name>"
+  exit
+fi
+
+$VBOX storageattach "$NAME" --storagectl ide --type dvddrive --medium "$VBOXADDITIONS" --port 0 --device 0 || die "Failed to attach VBoxAdditions"
+
