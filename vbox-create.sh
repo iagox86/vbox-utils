cefffa6b3b1dc16ae197555fa5d1ee1d4e81dcff
Ron <ron@skullsecurity.net>
2012-09-20 12:29:38 -0500

    Fixed some error handling, added snapshotting, changed scsi to ide to support win2000 more easily

--- vbox-create.sh
+++ vbox-create.sh
@@ -15,6 +15,7 @@ BRIDGE="eth0" # Set the bridged interface to eth0
 die()
 {
   $VBOX unregistervm "$NAME" --delete
+  rm "$HDD_FILE"
   echo "ERROR: ${1}"
   exit
 }
@@ -33,35 +34,60 @@ if [ $# -ne 2 ]; then
   exit
 fi
 
-# Create the directory where we're gonna be storing the harddrive
-/bin/mkdir -p "$VM_DIR"
-
+echo ">>> Checking if this VM already exists..."
 $VBOX showvminfo "$NAME" > /dev/null 2>&1 && alreadyexists
 
+# Create the directory where we're gonna be storing the harddrive
+echo ">>> Creating a directory for the VM $VM_DIR..."
+/bin/mkdir -p "$VM_DIR"
+
 # Create and register the VM
+echo ">>> Creating and registering the VM $NAME..."
 $VBOX createvm --name "$NAME" --register || die "Failed to create VM"
 
 # Set the memory to $MEMORY
+echo ">>> Setting the memory to $MEMORY"
 $VBOX modifyvm "$NAME" --memory $MEMORY || die "Failed to set memory"
 
 # Set the first nic as bridged to eth0
+echo ">>> Creating a network bridge to $BRIDGE..."
 $VBOX modifyvm "$NAME" --nic1 bridged --bridgeadapter1 $BRIDGE || die "Failed to set NIC"
 
 # Create a SCSI and a IDE interface
-$VBOX storagectl "$NAME" --name scsi --add scsi || die "Failed to create SCSI interface}"
+#$VBOX storagectl "$NAME" --name scsi --add scsi || die "Failed to create SCSI interface}"
+echo ">>> Creating an IDE interface..."
 $VBOX storagectl "$NAME" --name ide  --add ide || die "Failed to create IDE interface"
 
 # Create a harddrive file
+echo ">>> Cearing a harddrive: $HDD_FILE..."
 $VBOX createhd --filename "$HDD_FILE" --size "$HDD_SIZE" || die "Failed to crate harddrive file: $HDD_FILE"
 
-# Attach the SCSI interface
-$VBOX storageattach "$NAME" --storagectl scsi --type hdd --medium "$HDD_FILE" --port 0 || die "Failed to attach hdd.vmi"
+# Attach the Harddrive
+echo ">>> Attaching the harddrive"
+$VBOX storageattach "$NAME" --storagectl ide --type hdd --medium "$HDD_FILE" --port 0 --device 0 || die "Failed to attach $HDD_FILE"
 
 # Attach the requested ISO
-$VBOX storageattach "$NAME" --storagectl ide --type dvddrive --medium "$ISO" --port 0 --device 0 || die "Failed to attach the cdrom ISO"
+echo ">>> Attaching $ISO..."
+$VBOX storageattach "$NAME" --storagectl ide --type dvddrive --medium "$ISO" --port 0 --device 1 || die "Failed to attach the cdrom ISO"
 
 # Turn the VM on, in the background
+echo "Starting the UI and waiting..."
 $VBOXUI --startvm "$NAME" --hostkey 306 308 320 > /dev/null 2>&1 &
+
+echo "--------------------------------------------------------------------------------"
+echo "Press <ENTER> when you're ready to take a base snapshot"
+echo "(You should do it the first time the login screen is up)"
+echo "(Press ctrl+alt to release mouse)"
+echo "--------------------------------------------------------------------------------"
+read
+echo ">>> Pausing the VM..."
+$VBOX controlvm "$NAME" pause || echo "Failed to pause"
+
+echo ">>> Taking snapshot..."
+$VBOX snapshot "$NAME" take "base install" || echo "Failed to take snapshot"
+
+echo ">>> Resuming the VM..."
+$VBOX controlvm "$NAME" resume || echo "Failed to resume"
 
 echo "--------------------------------------------------------------------------------"
 echo "Press <ENTER> when you're ready to install drivers"
@@ -69,5 +95,6 @@ echo "(Press ctrl+alt to release mouse)"
 echo "--------------------------------------------------------------------------------"
 read
 
-$VBOX storageattach "$NAME" --storagectl ide --type dvddrive --medium "$VBOXADDITIONS" --port 0 --device 0 || die "Failed to attach VBoxAdditions"
+echo "Attaching the ISO $VBOXADDITIONS..."
+$VBOX storageattach "$NAME" --storagectl ide --type dvddrive --medium "$VBOXADDITIONS" --port 0 --device 1 || echo "Failed to attach the cdrom iso"
 
